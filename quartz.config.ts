7fa1c4358ef6a5d0cd256bd8edd834ff5cda13bf
nikitazaichenko <61910930+nikitazaichenko@users.noreply.github.com>
2026-02-02 14:28:42 +0300

    Update quartz.config.ts

--- quartz.config.ts
+++ quartz.config.ts
@@ -8,7 +8,7 @@ import * as Plugin from "./quartz/plugins"
  */
 const config: QuartzConfig = {
   configuration: {
-    pageTitle: "Quartz 4",
+    pageTitle: "Iservice",
     pageTitleSuffix: "",
     enableSPA: true,
     enablePopovers: true,
